1e870b50dd96211aa449b7a634ce6fcecc8c1b7d
PedroFerreiraaaa <aluno26624@mtorga.edu.pt>
2024-10-22 10:49:59 +0100

    Added net users and updated .gitignore

--- network.bat
+++ network.bat
@@ -17,8 +17,9 @@ echo 4 - Show MAC Address
 echo 5 - Energy Efficient
 echo 6 - File/App Association
 echo 7 - Disc Repair
-echo 8 - Ping to an URL
-echo 9 - Exit
+echo 8 - Ping to URL
+echo 9 - Users to File
+echo 10 - Exit
 set /p opt="Select an option then press ENTER: "
 if %opt% == 1 goto IP_CONFIGURATIONS
 if %opt% == 2 goto Renew_IP_CONFIGURATIONS
@@ -28,6 +29,7 @@ if %opt% == 5 goto Energy_Efficient
 if %opt% == 6 goto File_APP_Assoc
 if %opt% == 7 goto Disc_Repair
 if %opt% == 8 goto Ping_To_URL
+if %opt% == 9 goto Users_To_File
 if %opt% == 9 goto EXIT_SCREEN
 
 goto MENU
@@ -74,6 +76,11 @@ goto MENU
 :Ping_To_URL
 set /p url="Insert URL: "
 ping %url%
+pause
+goto MENU
+
+:Users_To_File
+net user > users.txt
 pause
 goto MENU
 
