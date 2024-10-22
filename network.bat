48d7883b7ae7b900790f8bc2c5b6cc70cb4735ad
PedroFerreiraaaa <aluno26624@mtorga.edu.pt>
2024-10-22 09:33:39 +0100

    Added File/App Assoc

--- network.bat
+++ network.bat
@@ -15,14 +15,16 @@ echo 2 - IP Renew
 echo 3 - DNS Query
 echo 4 - Show MAC Address
 echo 5 - Energy Efficient
-echo 6 - Exit
+echo 6 - File/App Association
+echo 7 - Exit
 set /p opt="Select an option then press ENTER: "
 if %opt% == 1 goto IP_CONFIGURATIONS
 if %opt% == 2 goto Renew_IP_CONFIGURATIONS
 if %opt% == 3 goto DNS_Query
 if %opt% == 4 goto Show_MAC_Address
 if %opt% == 5 goto Energy_Efficient
-if %opt% == 6 goto EXIT_SCREEN
+if %opt% == 6 goto File_APP_Assoc
+if %opt% == 7 goto EXIT_SCREEN
 
 goto MENU
 
@@ -52,6 +54,11 @@ goto MENU
 
 :Energy_Efficient
 powercfg /energy
+pause
+goto MENU
+
+:File_APP_Assoc
+assoc >> OUTPUT.txt
 pause
 goto MENU
 
